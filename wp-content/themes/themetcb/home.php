3b2da531648a8c808e3622b4d4204b7de925c888
Kendall <kadkins@thecipherbrief.com>
2017-10-17 15:25:22 -0400

    too long without a commit - includes ad 1 and small css changes to site

--- wp-content/themes/themetcb/home.php
+++ wp-content/themes/themetcb/home.php
@@ -30,7 +30,7 @@
         <div id="our_latest_news">
           <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'thecipherbrief-featured-image' ); ?></a>
           <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-          <h5><a class="author_name" href="<?php the_permalink(); ?>"> The Cipher Brief Staff </a></h5>
+          <h5><a class="author_name" href="<?php the_permalink(); ?>"> <?php the_author(); ?> </a></h5>
         </div>
       <?php endwhile; ?>
       <!-- end of the loop -->
@@ -85,7 +85,8 @@
 
 
     <!-- Threats -->
-    <div class='col' id="threat-report">
+    <div class="col col-threat">
+    <div id="threat-report">
       <h1 class="col-title"><a href="/category/threat-report">THREAT Report</a></h1>
       <?php
   // the query
@@ -122,6 +123,8 @@
   <?php endif; ?>
   <!-- <a href="/category/threat-report" class="view_more">VIEW MORE</a> -->
     </div>
+    <div class="sidebar_ad">AD SPACE 250x400</div>
+  </div>
   </div>
 
   <!-- Cipher Take -->
@@ -160,6 +163,7 @@
   </div>
 </div>
 
+<div class="">
   <div class="third-section">
     <!-- The White Papers -->
     <h1 class="col-title"><a href="/whitepaper">WHITE PAPERS</a></h1>
@@ -170,7 +174,7 @@
           $wpb_all_query = new WP_Query(array(
             'post_type'=> 'whitepapers',
             'post_status'=> 'publish',
-            'posts_per_page'=> 6,
+            'posts_per_page'=> 3,
             'orderby' => 'date',
             'order' => 'DESC'
           ));
@@ -195,15 +199,16 @@
           <?php endif; ?>
         </div>
 
+        <!-- Meet Our Board -->
         <div class="meet_board">
           <h3 style="font-weight: bold; text-align: center; line-height: 1.2; font-family: 'replica-pro';"><a href="https://www.thecipherbrief.com/cyber-advisory-board">Meet Our Board</a></h3>
-          <div class="meet_content" style='padding: 4px 4px 0 4px;'>
+          <div class="meet_content" style="padding: 4px 4px 0 4px;">
             <img class="board_img" src="https://thecipherbriefcyber.com/wp-content/uploads/2017/10/Robert-Work-bw.jpg" alt="Robert Work">
             <p class="board_text">Mr. Robert Work was the 31st Deputy Secretary of Defense, serving under three different Secretaries across two administrations. Previously, he served as chief executive officer of the Center for a New American Security. From 2009 to 2013, he served as the Under Secretary of the Navy. Mr. Work served on active duty in the U.S. Marine Corps for 27 years, retiring as a Colonel in 2001.</p>
           </div>
-
-              <a class='board_link' href="https://www.thecipherbrief.com/cyber-advisory-board" style=" display: block; text-align: center; color: #d52340;">view the rest of the Cyber Advisory Board</a>
-
+          <div class="board_link_holder">
+              <a class="board_link" href="https://www.thecipherbrief.com/cyber-advisory-board" style=" display: block; text-align: center; color: #d52340;">view the rest of the Cyber Advisory Board</a>
+          </div>
         </div>
 
       </div>
@@ -213,7 +218,7 @@
 
   </div>
 
-  <!-- extraneous blocks -->
+  <!-- Promo Blocks -->
   <div class="holder-container">
     <div class="promo_blocks">
       <h3>How to Subscribe</h3>
@@ -228,6 +233,7 @@
       <p>Do you agree with the Board's findings? Disagree? Let us know your point of view and be a part of the conversation. We'll publish the most relevant subscriber contributions in the next issue. Subscribers can email us at <a href="mailto:cyberadvisoryboard@thecipherbrief.com">cyberadvisoryboard@thecipherbrief.com</a>.</p>
     </div>
   </div>
+</div>
 </div>
 </div>
 
